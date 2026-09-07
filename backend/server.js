fb9571aece031812f17c0e5d247fae2b5e502e47
jitendra <jitendrasingh63793@gmail.com>
2026-09-07 20:15:20 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ const PORT = 5000;
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    // origin: 'http://localhost:3000',
+      origin:  'https://quickcart-frontend-02pg.onrender.com',
     credentials: true,
 }));
 app.use(cookieParser());
